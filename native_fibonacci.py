706c9a1a529a68b7517043956f746b9e2135af94
Subhan Ahmed <subhanahmed0987654@gmail.com>
2025-03-24 21:58:02 +0500

    Added Fibonacci script

--- native_fibonacci.py
+++ native_fibonacci.py
@@ -2,19 +2,12 @@ def fibonnaci(number):
     count = 1
     first_num = 0
     second_num = 1
-    
-    if number == 0:
-        return 0
-    elif number == 1:
-        return 1
-    else:
-        while count<number:
-            print(first_num,end=" ")
-            new_number = second_num + first_num
-            first_num = second_num
-            second_num = new_number
-            count+=1
+    while count<number:
+        print(first_num,end=" ")
+        new_number = second_num + first_num
+        first_num = second_num
+        second_num = new_number
+        count+=1
+        
             
 fibonnaci(16)
-
-    
